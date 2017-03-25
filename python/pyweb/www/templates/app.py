f890239c3b48df04a8f029b64bbb6b79ef5700ec
ma-lijun <343190282@qq.com>
2017-03-26 00:43:35 +0800

    forum 打开是下载页面

--- python/pyweb/www/templates/app.py
+++ python/pyweb/www/templates/app.py
@@ -1,4 +1,6 @@
-import logging;logging.basicConfig(level=logging.INFO)
+__author__ = 'ma_lijun'
+
+import logging; logging.basicConfig(level=logging.INFO)
 
 import  asyncio,os,json,time
 from datetime import datetime
@@ -7,11 +9,11 @@ from aiohttp import web
 def index(request):
     return web.Response(body=b'<h1>forum</h1>')
 
-@asyncio.coroutine
+asyncio.coroutine
 def init(loop):
     app=web.Application(loop=loop)
     app.router.add_route('GET','/',index)
-    srv=yield from loop.creat_server(app.make_handler(),'127.0.1',9000)
+    srv=yield from loop.create_server(app.make_handler(),'127.0.1',9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
