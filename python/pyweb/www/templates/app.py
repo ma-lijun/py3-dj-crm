566eac90409d4b9b3705169545fa2ebe022c8f02
ma-lijun <343190282@qq.com>
2017-03-26 00:47:53 +0800

    forum 打开是下载页面,终于调通了，但是不理解

--- python/pyweb/www/templates/app.py
+++ python/pyweb/www/templates/app.py
@@ -7,14 +7,14 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>forum</h1>')
+    return web.Response(body=b'<h1>forum</h1>', content_type='text/html', charset='UTF-8')#新增加编码，确切为编码类型html
 
 asyncio.coroutine
 def init(loop):
     app=web.Application(loop=loop)
     app.router.add_route('GET','/',index)
-    srv=yield from loop.create_server(app.make_handler(),'127.0.1',9000)
-    logging.info('server started at http://127.0.0.1:9000...')
+    srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',9002)
+    logging.info('server started at http://127.0.0.1:9001...')
     return srv
 
 loop=asyncio.get_event_loop()
